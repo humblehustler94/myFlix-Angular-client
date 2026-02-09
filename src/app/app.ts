fd00d4a312f3710f8e478951e6dc4286649afa6e
humblehustler94 <flores.itzel94@gmail.com>
2026-02-09 14:29:28 -0800

    feat: integrate material design, setup animations in app.config, and import UI modules to AppComponent
    Updated:
    - src\app\app.config.ts
    - src\app\app.ts
    
    other files updated after install @angular/material
    - src\index.html
    - src\styles.scsss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// --- ADD THESE MATERIAL IMPORTS ---
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+// ----------------------------------
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    // --- ADD THE MODULES HERE ---
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule
+    // ---------------------------
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
