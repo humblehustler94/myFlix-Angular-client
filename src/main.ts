fe5e32529ec9a4847a13305a75c4c5ad37de56ac
humblehustler94 <flores.itzel94@gmail.com>
2026-02-20 10:58:19 -0800

    style: center welcome page content and fix main.ts import
    - npm run generate-docs
    - new folder created named docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app';
 
 /**
  * Entry point for the myFlix Angular application.
  * @description This file is responsible for bootstrapping (starting) the Angular application.
  * It initializes the root component 'App' using the global configurations defined in 'appConfig'.
  */
-bootstrapApplication(App, appConfig)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
